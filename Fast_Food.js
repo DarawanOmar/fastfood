abea0a48d82f77f6d7d41941c445c0a8b4e12eea
DarawanOmar <darait721@gmail.com>
2023-01-22 00:05:14 +0300

    upload project

---
+++ Fast_Food.js
@@ -0,0 +1,13 @@
+function show()
+{
+    document.getElementById('content').style.display="block"
+    document.getElementById('content').style.height="20px"
+    document.getElementById('show').style.display="none"
+}    
+
+function hide()
+{
+    document.getElementById('content').style.display="none"
+    document.getElementById('content').style.height="0px"
+    document.getElementById('show').style.display="inline"
+}    
